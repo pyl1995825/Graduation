5f0c274f8653ca73d3fd00f21df61c83fafa09b9
pyl1995825 <604512714@qq.com>
2016-03-21 19:48:25 +0800

    anthoer test fenzhi

---
+++ GraduationDesignSystem/src/com/cn/nefu/controller/PYLTest.java
@@ -0,0 +1,11 @@
+package com.cn.nefu.controller;
+
+public class PYLTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println(a);
+	}
+
+}
