c1ef1a98f7098a0a9f3c9d3fe9e07c05748815a5
pyl1995825 <604512714@qq.com>
2016-03-22 14:17:01 +0800

    彭堉林的分支测试2

--- GraduationDesignSystem/src/com/cn/nefu/controller/Demo.java
+++ GraduationDesignSystem/src/com/cn/nefu/controller/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
 		// TODO Auto-generated method stub
 		System.out.println("1523");
 		System.out.println("123");
+		System.out.println("pengyulin");
 	}
 	
 	public static void getName() {
