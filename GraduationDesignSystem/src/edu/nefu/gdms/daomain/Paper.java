626b959fbc58271a82302fbacc6bb38561bffd8d
Ghost-Pz <1025553816@qq.com>
2016-03-23 21:53:16 +0800

    lyn创建实体

---
+++ GraduationDesignSystem/src/edu/nefu/gdms/daomain/Paper.java
@@ -0,0 +1,51 @@
+package edu.nefu.gdms.daomain;
+
+public class Paper {
+	private String pid;
+	private String name;
+	private String filepath;
+	private String sid;
+	private double score;
+	private String stid;
+	
+	
+	
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFilepath() {
+		return filepath;
+	}
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public String getStid() {
+		return stid;
+	}
+	public void setStid(String stid) {
+		this.stid = stid;
+	}
+	
+	
+}
---
+++ GraduationDesignSystem/src/edu/nefu/gdms/daomain/Teacher.java
@@ -0,0 +1,85 @@
+package edu.nefu.gdms.daomain;
+
+public class Teacher {
+	/*
+	 * 
+	 * 
+	 * 
+	 */
+
+	private String teid;
+	
+	private String number;
+	
+	private String name;
+	
+	private String pwd;
+	
+	private String phone;
+	
+	private String email;
+	
+	private String major;
+	
+	private String gid;
+	
+	private String level;
+	
+
+	public String getTeid() {
+		return teid;
+	}
+	public void setTeid(String teid) {
+		this.teid = teid;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getGid() {
+		return gid;
+	}
+	public void setGid(String gid) {
+		this.gid = gid;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	
+	
+}
